e91d8761993d6a35e479839c92e3efb9d1897179
freditya <freditya12@gmail>
2019-06-12 23:10:49 +0700

    database

--- src/main/java/etracer/com/projectetracer/config/WebSecurityConfig.java
+++ src/main/java/etracer/com/projectetracer/config/WebSecurityConfig.java
@@ -92,6 +92,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/email/**").permitAll() 
                 .antMatchers("/assets/**").permitAll() 
                 .antMatchers("/confirm-account/**").permitAll() 
+                .antMatchers("/forgot-password/**").permitAll() 
                 .antMatchers("/reset-password/**").permitAll() 
                 .antMatchers("/admin/**","/employee/**").hasAnyAuthority("ADMIN")
                 .antMatchers("/manager/**").hasAnyAuthority("MANAGER")
